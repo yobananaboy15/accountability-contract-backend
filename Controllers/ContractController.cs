026fb2f608f07c015057fad7bf729395d01b0d0a
Axel Thiel <axel.thiel@ica.se>
2024-08-01 13:05:53 +0200

    Update ContractController

--- Controllers/ContractController.cs
+++ Controllers/ContractController.cs
@@ -23,6 +23,31 @@ namespace AccountabilityApp.Controllers
         }
 
         [HttpGet("{id}")]
-        //
+        public async Task<IActionResult> GetContract(int id)
+        {
+            Contract contract = await _context.Contracts.FindAsync(id);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+            return Ok(contract);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateContract(CreateContractDTO model)
+        {
+            var contract = new Contract { CreatedAt = DateTime.UtcNow, StartDate = model.StartDate, EndDate = model.EndDate, Title = model.Title, Content = model.Content, UpdatedAt = DateTime.UtcNow, CreatedBy = 1 };
+
+            _context.Contracts.Add(contract);
+            await _context.SaveChangesAsync();
+
+            return Ok(contract);
+        }
+
+        [HttpPut("{id}")]
+        //TODO: implement this
+        public async Task<IActionResult> UpdateContract()
+        {
+            return Ok();
+        }
     }
 }
