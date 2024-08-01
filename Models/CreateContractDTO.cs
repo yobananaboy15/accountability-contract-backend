2585ff00fa246b6cbde42ef00c7426dba27f90ef
Axel Thiel <axel.thiel@ica.se>
2024-08-01 13:06:06 +0200

    Update some DTO models

--- Models/CreateContractDTO.cs
+++ Models/CreateContractDTO.cs
@@ -1,16 +1,10 @@
 ﻿namespace AccountabilityApp.Models
 {
-    public class Contract
+    public class CreateContractDTO
     {
-        public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
-        public int CreatedBy { get; set; }
-        public DateTime CreatedAt { get; set; }
-
-        public DateTime UpdatedAt {  get; set; } 
         public string StartDate { get; set; }
         public string EndDate { get; set; }
-
     }
 }
