f3e38e44fbecb7b03dc15d73c6e6cdfdd90b09d5
Axel Thiel <axel.thiel@ica.se>
2024-08-14 08:50:58 +0200

    Add possibility to sign signature using a token

--- Models/Signature.cs
+++ Models/Signature.cs
@@ -6,9 +6,7 @@
         public int ContractId { get; set; }
         public int? UserId { get; set; }
         public DateTime? SignedDate { get; set; }
-
         public string Token { get; set; }
-
         public DateTime ExpirationDate { get; set; }
     }
 }
